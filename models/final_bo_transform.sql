f2c2a9d7497572058e6030bbda7568764150d997
souravmoh <96616008+souravmoh@users.noreply.github.com>
2022-01-28 17:38:16 +0530

    Update final_bo_transform.sql

--- models/final_bo_transform.sql
+++ models/final_bo_transform.sql
@@ -1,10 +1,10 @@
-{% macro transformation_1(col1,col2) %}
+{% macro bo_1(col1,col2) %}
     (case
     when {{col1 }} is not null and {{col2}}=2 then 100-{{col1}}
     else {{col1}}
     end)
 {% endmacro %}
-{% macro transformation_2(col3,col4) %}
+{% macro bo_2(col3,col4) %}
     (case
     when {{col3}}='Y' and {{col4}}='Y' then 'BEARREG'
     when {{col3}}='Y' and ({{col4}}='N' or {{col4}} is null) then 'BEARER'
@@ -12,7 +12,7 @@
     else 'UNKNOWN'
     end)
 {% endmacro %}
-{% macro transformation_3_and_4(col5,col6,col7) %}
+{% macro bo_3_and_4(col5,col6,col7) %}
     (case
     when {{col5}}='MBONO' then {{col7}}/100
     when {{col5}} in ('BLFT','BLTN','BNTNB','BNTNC','BNTNF') then {{col7}}/1000
@@ -20,23 +20,23 @@
     else {{col7}}
     end)
 {% endmacro %}
-{% macro transformation_5(col8,col9) %}
+{% macro bo_5(col8,col9) %}
 (case
  when {{col8 }} is not null then {{col8}}
  when {{col9}}='N' then 1 
  when {{col9}}='Y' then 100
  end)
 {% endmacro %}
-{% macro transformation_6(col10) %}
+{% macro bo_6(col10) %}
 (case
  when {{col10}}='Y' then 'FULLY'
  else 'INCOMPLETE'
  end)
 {% endmacro %}
-select *,{{transformation_1('ISSUE_PX','PCS_QUOTE_TYP')}} as OfferPrice,
-{{transformation_2('BEARER','REGISTERED')}} as SecurityFormType,
-{{transformation_3_and_4('TICKER','PAR_AMT','MIN_PIECE')}} as minimumTradeSize,
-{{transformation_3_and_4('TICKER','PAR_AMT','MIN_INCREMENT')}} as RoundLotSize,
-{{transformation_5('PAR_AMT','PCT_PAR_QUOTED')}} as NominalValueOfUnit,
-{{transformation_6('"CALLED "')}} as calleventtype
+select *,{{bo_1('ISSUE_PX','PCS_QUOTE_TYP')}} as OfferPrice,
+{{bo_2('BEARER','REGISTERED')}} as SecurityFormType,
+{{bo_3_and_4('TICKER','PAR_AMT','MIN_PIECE')}} as minimumTradeSize,
+{{bo_3_and_4('TICKER','PAR_AMT','MIN_INCREMENT')}} as RoundLotSize,
+{{bo_5('PAR_AMT','PCT_PAR_QUOTED')}} as NominalValueOfUnit,
+{{bo_6('"CALLED "')}} as calleventtype
 from destination.public.final_bo
